31909c7f51a447d9739da8b18f2d8003fceb80cc
Vincent Robert <vincent.robert@nanego.com>
2015-07-09 17:30:18 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Redmine Autocomplete Subject plugin
+=======================
+
+Autofill the subject of the new issue when the user select the tracker, the category or the other issue fields.
+
+
---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_autocomplete_subject do
+  name 'Redmine Autocomplete Subject plugin'
+  author 'Vincent ROBERT'
+  description 'This is a plugin for Redmine'
+  version '1.0.0'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
