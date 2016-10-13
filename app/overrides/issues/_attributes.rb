a29f3f90002c7069fa568e6d92acb8915c2bbb73
Vincent Robert <vincent.robert@nanego.com>
2016-10-13 16:43:44 +0200

    Improve compatibility with plugin Customize Core Fields

--- app/overrides/issues/_attributes.rb
+++
@@ -1,9 +0,0 @@
-Deface::Override.new :virtual_path => 'issues/_form',
-                     :name         => 'insert_js_script_for_autocompleted_fields',
-                     :insert_before => 'erb[loud]:contains("javascript_tag")',
-                     :text      => <<EOD
-<%= javascript_tag do %>
-  <%= render :partial => "issues/autocomplete_subject.js" %>
-<% end if @issue.project.module_enabled?("autocomplete_subject")%>
-
-EOD
