65b38145bd99c5b267ed2daba4a2da07ff8bee5f
Vincent <contact@vincent-robert.com>
2025-07-21 15:04:06 +0200

    Include missing fixtures in specs

--- spec/controllers/issues_controller_spec.rb
+++ spec/controllers/issues_controller_spec.rb
@@ -1,7 +1,9 @@
 require "spec_helper"
 
 describe IssuesController, :type => :controller do
-  fixtures :users, :projects, :trackers, :issues, :enabled_modules, :custom_fields, :custom_values
+  fixtures :users, :projects, :trackers, :issues,
+           :enabled_modules, :custom_fields, :custom_values,
+           :enumerations, :issue_statuses, :members, :roles, :issue_categories, :member_roles
 
   let(:project) { Project.find(1) }
   let(:tracker) { Tracker.find(1) }
@@ -10,6 +12,7 @@ describe IssuesController, :type => :controller do
   before do
     @request.session[:user_id] = user.id
     project.enabled_module_names = project.enabled_module_names | ["autocomplete_subject"]
+    project.trackers << tracker unless project.trackers.include?(tracker)
     project.autocomplete_separator = "-"
     project.show_subject_input = 1
     project.autocomplete_subject_tracker_ids = "1"
@@ -19,60 +22,76 @@ describe IssuesController, :type => :controller do
   end
 
   it "generates the subject when module is activated and config is set" do
-    post :create, :params => {
-      :project_id => project.id,
-      :issue => {
-        :tracker_id => tracker.id,
-        :author_id => user.id,
-        :description => "Test description"
+    expect {
+      post :create, :params => {
+        :project_id => project.id,
+        :issue => {
+          :tracker_id => tracker.id,
+          :author_id => user.id,
+          :status_id => 1,
+          :priority_id => 4,
+          :description => "Test description"
+        }
       }
-    }
-    issue = Issue.last
+    }.to change { Issue.count }.by(1)
+    issue = Issue.order("id DESC").first
     expect(issue.subject).to eq("John Smith-Bug") # [user.name, tracker.name].join("-")
   end
 
   it "does not change the subject if autocompletion is DISABLED in project's settings" do
     project.update!(show_subject_input: 3) # Disable autocompletion
-    post :create, :params => {
-      :project_id => project.id,
-      :issue => {
-        :tracker_id => tracker.id,
-        :author_id => user.id,
-        :description => "Test description",
-        :subject => "Subject added by the user"
+    expect {
+      post :create, :params => {
+        :project_id => project.id,
+        :issue => {
+          :tracker_id => tracker.id,
+          :author_id => user.id,
+          :status_id => 1,
+          :priority_id => 4,
+          :description => "Test description",
+          :subject => "Subject added by the user"
+        }
       }
-    }
-    issue = Issue.last
+    }.to change { Issue.count }.by(1)
+    issue = Issue.order("id DESC").first
     expect(issue.subject).to eq("Subject added by the user")
   end
 
   it "does not change the subject if the functionality is disabled for current tracker" do
     project.update!(autocomplete_subject_tracker_ids: "2")
-    post :create, :params => {
-      :project_id => project.id,
-      :issue => {
-        :tracker_id => tracker.id,
-        :author_id => user.id,
-        :description => "Test description",
-        :subject => "Subject added by the user"
+    expect {
+      post :create, :params => {
+        :project_id => project.id,
+        :issue => {
+          :tracker_id => tracker.id,
+          :author_id => user.id,
+          :status_id => 1,
+          :priority_id => 4,
+          :description => "Test description",
+          :subject => "Subject added by the user"
+        }
       }
-    }
-    issue = Issue.last
+    }.to change { Issue.count }.by(1)
+    issue = Issue.order("id DESC").first
     expect(issue.subject).to eq("Subject added by the user")
   end
 
   it "adds the start_date to the subject when configured" do
     AutocompletedField.create!(project_id: project.id, field_object: "Issue", field_name: "issue_start_date", position: 2)
-    post :create, :params => {
-      :project_id => project.id,
-      :issue => {
-        :tracker_id => tracker.id,
-        :author_id => user.id,
-        :start_date => "2024-06-01",
-        :description => "Test avec date de début"
+    expect {
+      post :create, :params => {
+        :project_id => project.id,
+        :issue => {
+          :tracker_id => tracker.id,
+          :author_id => user.id,
+          :status_id => 1,
+          :priority_id => 4,
+          :start_date => "2024-06-01",
+          :description => "Test avec date de début"
+        }
       }
-    }
-    issue = Issue.last
+    }.to change { Issue.count }.by(1)
+    issue = Issue.order("id DESC").first
     expect(issue.subject).to eq("John Smith-Bug-2024-06-01")
   end
 end
